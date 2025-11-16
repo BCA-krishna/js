264c731188b54a722231b04f85379aac6488c448
Krishna Patel <krishnapatel1284@gmail.com>
2025-11-16 10:29:14 +0000

    operators

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,8 @@
+// "use strict"; // treate all js code as newer version
+// alert(3+3)//we are using nodejs ,not browser
+let myname = "krishna"
+let age = 18
+let isindian = true
+let income = null
+let ab;
+console.table([myname,age,isindian,income,ab]);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+// let score=null
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueinnumber=Number(score)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber);
+
+
+// let isloggedin=1
+// let booleanisloggedin=Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+// let num="33"
+// let stringnum=String(num)
+// console.log(stringnum);
+// console.log(typeof stringnum);
+
+
