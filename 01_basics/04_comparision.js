b8d1e62c699e26feba56603bc91fde2926fb6da2
Krishna Patel <krishnapatel1284@gmail.com>
2025-11-22 07:59:24 +0000

    comparision operator

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,28 @@
+// console.log("2">1)
+// console.log("02">1)
+
+// in js comparison check and equality check both  are the different
+
+// console.log(null>0)
+// console.log(null == 0)
+// console.log(null >=0)
+
+// === check value as well as data type
+// if("2"===2){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
+// if("2"==2){
+//     console.log(true);
+    
+// }
+// else{
+//     console.log(false);
+    
+// }
+
