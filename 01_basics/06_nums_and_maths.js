27f9ea657f5c09a518fdfc554074e4c099f93640
Krishna Patel <krishnapatel1284@gmail.com>
2025-11-22 07:59:46 +0000

    maths function

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,35 @@
+const score=400
+// console.log(score);
+const balance=new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber=23.8966
+// console.log(otherNumber.toFixed(2));
+const hundred=1000000
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+///////////////// maths//////////////////////
+// console.log(Math)
+// console.log(Math.abs(-11));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.pow(20,2))
+
+// console.log((Math.random()*10)+1)
+// console.log((Math.random()*10)+1)
+
+const min=1
+const max=6
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
+
