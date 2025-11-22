3d2beba881eea0bc53f409ac442bf0356f6d02d6
Krishna Patel <krishnapatel1284@gmail.com>
2025-11-22 05:18:30 +0000

    intoduction of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail ="krishnapatel@123"
+var accountpass="1234"
+accountcity="mathura"
+let accountstate;
+// accountId=2
+
+accountEmail="avx@as"
+accountpass="456"
+accountcity="vbn"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpass,accountcity,accountstate])
--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,29 @@
 // console.log(typeof stringnum);
 
 
+//*******************OPRATION*****************************
+// let value=3
+// let negvalue=-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(5%3)
+
+// let str1="hello"
+// let str2=" krishna"
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+let gamec=100
+++gamec
+console.log(gamec);
+
