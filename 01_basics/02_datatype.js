6fb8be0d7eb9c4fb7856a3e3a7f49855cb501a00
Krishna Patel <krishnapatel1284@gmail.com>
2025-11-22 07:58:08 +0000

    implementing strings method

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,8 +1,26 @@
 // "use strict"; // treate all js code as newer version
 // alert(3+3)//we are using nodejs ,not browser
-let myname = "krishna"
-let age = 18
-let isindian = true
-let income = null
-let ab;
-console.table([myname,age,isindian,income,ab]);
+// let myname = "krishna"
+// let age = 18
+// let isindian = true
+// let income = null
+// let ab;
+// console.table([myname,age,isindian,income,ab]);
+
+// stack (premitive) , heap(non premitive)
+// let name1="krishna"
+// let name2=name1
+// name2="krish"
+// console.log(name1);
+// console.log(name2);
+
+
+// let address = {
+//     city:"mathura",
+//     pincode:281121
+// }
+// console.log(address);
+// let address2=address
+// address.pincode=111111
+// console.log(address.pincode);
+// console.log(address2.pincode)
---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+// const name="krishna"
+// const age=18
+// console.log(name + age);
+// console.log(`hello my name is ${name} and i am  ${age}  year old`);
+
+// const name1=new String("google_Company")
+// console.log(name1[0]);
+// console.log(name1.length);
+// console.log(name1.toUpperCase());
+// console.log(name1.charAt(0));
+// console.log(name1.indexOf("l"));
+// console.log(name1.substring(0,4));
+
+// const str1="      krishna      "
+// console.log(str1);
+// console.log(str1.trim())
+
+// const url="https://krishna.com/krishna%20patel"
+
+// console.log(url.replace("%20",''));
+// console.log(url.includes("krishna"));
+// console.log(name1.split("_"));
+// console.log(name1.bold());
+
+
+
+
+
+
+
+
+
+
+
+
